78a521f4adecb5178868af37201cf1c8dad0a433
mjrizzo22 <mjrizzo22@gmail.com>
2021-01-16 23:59:21 -0500

    Day 13: lottoCheck and web-scrapping

--- lottoCheck.py
+++ lottoCheck.py
@@ -4,40 +4,51 @@
 
 # Assumes games are 5 regular numbers from 1-70
 # Jackpot numbers is 1 integer from 1-26
-def valid_input(x):
+def valid_game_input(x):
     while(True):
         try:
-            game_num = int(input("Enter game numbers:\n"))
-            if game_num not in range(1,x):
-                print("Input must be in range, try again:")
+            game_num = int(input())
+            if game_num not in range(1, (x+1)):
+                print("Input must be in range, try again:\n")
                 continue
             break 
         except:
-            print("That was not a game number, try again:")
+            print("That was not a game number, try again:\n")
     return game_num
+
 def getGame():
     x = {}
-    print("Enter the regular winning, 1-70")
+    print("First, regular numbers, 1-70:")
     for i in range(5):
-        x[i] = valid_input(70)
-    print("Enter the jackpot number, 1-26")
-    x[5] = valid_input(26)
+        x[i] = valid_game_input(70)
+    print("Second, the jackpot number, 1-26:")
+    x[5] = valid_game_input(26)
     return x
 
-# step 2
-def checkGame():
-    # check if jackpot number
-    # check check sorted non jackpot numbers
-    # return row/game # and win
-    return 0
+def checkGame(x, y):
+    matches = {k: x[k] for k in x if k in y and x[k] == y[k]}
+    l = len(matches)
+
+    if l == 0:
+        return "Not a winner, sorry!"
+    elif l == 6:
+        return "JACKPOT"
+
+    output = ""
+    if 5 in matches:
+        output = " And the Jackpot Number!"
+        l = l - 1 
+    output = "You matched "+str(l)+" regular numbers!"+output
+    return output
 
 if __name__ == "__main__":
-    print("Check if you are winner!\n")
-    # Step 0: get winning numbers
+    print("Check if you are a winner!\n")
+
+    print("Enter the winning numbers:\n")
     winning_game = getGame()
-    # Step 1: get 2d array of user games
-    # rows are a game
-    # Step 2.0-2.8: search winning conditions
-    # split conditions by jackpot
-    # Step 3: output winning game and total match
+
+    print("Enter your numbers:\n")
+    user_game = getGame()
+
+    print(checkGame(winning_game, user_game))
     exit()
