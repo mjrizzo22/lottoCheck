d3b8332ec4e2f04d2c91c3f5abf0fdd3e39e502b
mjrizzo22 <mjrizzo22@gmail.com>
2021-01-18 21:51:02 -0500

    Day 14: reusable

--- lottoCheck.py
+++ lottoCheck.py
@@ -4,8 +4,22 @@
 
 # Assumes games are 5 regular numbers from 1-70
 # Jackpot numbers is 1 integer from 1-26
+import distutils
+from distutils import util
+
+def query():
+    question = "Do you want to enter a .csv? [y/n]:\n"
+
+    while True:
+        try:
+            resp = input(question).strip().lower()
+            return distutils.util.strtobool(resp)
+        except ValueError:
+            print(
+                "Please respond with 'yes' or 'no' (or 'y' or 'n').\n") 
+
 def valid_game_input(x):
-    while(True):
+    while True:
         try:
             game_num = int(input())
             if game_num not in range(1, (x+1)):
@@ -16,7 +30,7 @@ def valid_game_input(x):
             print("That was not a game number, try again:\n")
     return game_num
 
-def getGame():
+def getSingleGame():
     x = {}
     print("First, regular numbers, 1-70:")
     for i in range(5):
@@ -25,30 +39,47 @@ def getGame():
     x[5] = valid_game_input(26)
     return x
 
-def checkGame(x, y):
+def getMatches(x, y):
     matches = {k: x[k] for k in x if k in y and x[k] == y[k]}
-    l = len(matches)
+    return matches
+
+def printWin(m):
+    l = len(m)
 
     if l == 0:
-        return "Not a winner, sorry!"
+        Print("Not a winner, sorry!")
     elif l == 6:
-        return "JACKPOT"
+        Print("JACKPOT")
 
     output = ""
-    if 5 in matches:
+    if 5 in m:
         output = " And the Jackpot Number!"
         l = l - 1 
     output = "You matched "+str(l)+" regular numbers!"+output
-    return output
+    
+    print(output)
 
-if __name__ == "__main__":
-    print("Check if you are a winner!\n")
+def checkSingle():
+    print("Check a single game:\n")
 
     print("Enter the winning numbers:\n")
-    winning_game = getGame()
+    winning_game = getSingleGame()
 
     print("Enter your numbers:\n")
-    user_game = getGame()
+    user_game = getSingleGame()
 
-    print(checkGame(winning_game, user_game))
+    matches = getMatches(winning_game, user_game)
+    printWin(matches)
+
+def checkSheet():
+    return 0
+
+if __name__ == "__main__":
+    print("Check if you are a winner!\n")     
+    
+    if query():
+        checkSheet()
+    else:
+        checkSingle()
+
     exit()
